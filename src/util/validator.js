dbb65c02608c09f20292c82be0012d72ed732180
bhuvneshindoriya <bhuvneshindoriya0909@gmail.com>
2022-12-21 18:41:42 +0530

    testing

--- src/util/validator.js
+++ src/util/validator.js
@@ -10,7 +10,7 @@ function isValidEmail(email) {
 }
 
 function isValidphone(phone) {
-    const re = /^(\+91[\-\s]?)?[0]?(91)?[789]\d{9}$/;
+    const re = /^(\+91[\-\s]?)?[0]?(91)?[6789]\d{9}$/;
     return re.test(String(phone));
 }
 const isValidBody = function (Stringvalue) {
@@ -23,7 +23,7 @@ const isValidBody = function (Stringvalue) {
 const isValidName = (value) => { return (/^[A-Z a-z]+$/).test(value); }
 const isValidpassword = (value) => { return (/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,15}$/.test(value)); }
 const isValidCity = (value) => { return (/^[A-za-z]+$/).test(value) }
-const isValidPinCode = (value) => { return (/^[1-9][0-9]{6}$/).test(value) }
+const isValidPinCode = (value) => { return (/^[1-9][0-9]{5}$/).test(value) }
 const isValidProductName = (value) => { return (/^[A-Za-z]+|[A-Za-z]+\[0-9]+$/).test(value) }
 const isValidPrice = (value) => { return (/^(?:0|[1-9]\d*)(?:\.(?!.*000)\d+)?$/).test(value) }
 const isValidateSize = (value) => { return ["S", "XS", "M", "X", "L", "XXL", "XL"].indexOf(value) !== -1 }
